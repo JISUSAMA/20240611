784c5ef6dd7b9ac56534eda5d09f4fe117480dfc
별찌루 <38304918+JISUSAMA@users.noreply.github.com>
2024-06-26 12:03:18 +0900

    init

--- src/p07_Collection/Ex09MapApplicate.java
+++ src/p07_Collection/Ex09MapApplicate.java
@@ -1,4 +1,30 @@
 package p07_Collection;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Ex09MapApplicate {
+  private static Map phoneBook = new HashMap();
+  public static void main(String[] args) {
+    addPhone("친구","김수한", "010-1111-1110");
+    addPhone("친구","김수한", "010-1111-1111");
+    addPhone("친구","김수한", "010-1111-1112");
+    addPhone("친구","김수한", "010-1111-1113");
+    addPhone("마트","010-1111-1114");
+
+    printBook(phoneBook);
+  }
+
+  private static void addPhone(String  type, String name, String number)
+  {
+
+  }
+  private static void addPhone(String type, String number)
+  {
+
+  }
+  private static void printBook(Map map)
+  {
+
+  }
 }
