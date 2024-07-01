284fc579a1806c9c7e3a158283ca9946741906a8
별찌루 <38304918+JISUSAMA@users.noreply.github.com>
2024-07-01 16:37:30 +0900

    init

---
+++ src/p12_database/ui/FrmBasic.java
@@ -0,0 +1,22 @@
+package p12_database.ui;
+
+import javax.swing.*;
+
+public abstract class FrmBasic extends JFrame {
+  private int width, height;
+  public FrmBasic(String title, int width, int heigth) {
+    this.width = width;
+    this.height = heigth;
+    init();
+    arrange();
+    inflate();
+  }
+  public abstract void init();
+  public abstract void arrange();
+  public void inflate() {
+    setSize(width, height);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setLocationRelativeTo(this);
+    setVisible(true);
+  }
+}
--- src/p12_database/ui/FrmLogin.java
+++ src/p12_database/ui/FrmLogin.java
@@ -1,7 +1,22 @@
 package p12_database.ui;
 
-public class FrmLogin {
-  public static void main(String[] args) {
+public class FrmLogin extends FrmBasic{
+  public FrmLogin(String title, int width, int heigth) {
+    super(title, width, heigth);
+  }
 
+  @Override
+  public void init() {
+
+  }
+
+  @Override
+  public void arrange() {
+
+  }
+
+  @Override
+  public void inflate() {
+    super.inflate();
   }
 }
