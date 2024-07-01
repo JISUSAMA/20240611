89544bf07ba21c040fe05ab2440d5213a374e87d
별찌루 <38304918+JISUSAMA@users.noreply.github.com>
2024-07-01 15:40:01 +0900

    init

--- src/p12_database/vo/Members.java
+++ src/p12_database/vo/Members.java
@@ -19,4 +19,35 @@ public class Members {
     this.name = name;
     this.mobile = mobile;
   }
+
+  public long getMno() {
+    return mno;
+  }
+  public void setMno(long mno) {
+    this.mno = mno;
+  }
+  public String getId() {
+    return id;
+  }
+  public void setId(String id) {
+    this.id = id;
+  }
+  public String getPass() {
+    return pass;
+  }
+  public void setPass(String pass) {
+    this.pass = pass;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getMobile() {
+    return mobile;
+  }
+  public void setMobile(String mobile) {
+    this.mobile = mobile;
+  }
 }
