830c8c80f0c5c47ee48d66473ecc1fe973f7be7a
별찌루 <38304918+JISUSAMA@users.noreply.github.com>
2024-07-01 16:41:15 +0900

    init

--- src/p12_database/ui/FrmLogin.java
+++ src/p12_database/ui/FrmLogin.java
@@ -1,8 +1,16 @@
 package p12_database.ui;
 
+import javax.swing.*;
+
 public class FrmLogin extends FrmBasic{
+  private JLabel lbLogin,lbID, lbPass;
+  private  JTextField tfId;
+  private JPasswordField pfPass;
+  private JButton btnLogin, btnJoin, btnClose;
+
   public FrmLogin(String title, int width, int heigth) {
     super(title, width, heigth);
+
   }
 
   @Override
